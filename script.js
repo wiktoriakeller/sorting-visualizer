2bc20126224c5e839190454bed8787680df91ebb
wiktoriakeller <wiktoriakeller552@gmail.com>
2022-02-18 20:35:30 +0100

    Add colors to visualization

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const container = document.getElementById("bars");
+const sortedBarColor = "#f5761a";
+const unsortedBarColor = "#ddb12c";
+const normalBarColor = "#996033";
 
 $(document).ready(function() {
     setSortSliderVal();
@@ -21,7 +23,7 @@ function setSortSliderVal() {
     let barWidth = width / sortRangeVal;
 
     for(let i = 0; i < sortRangeVal; i++) {
-        let randomHeight = getRndInteger(minHeight, maxHeight);
+        let randomHeight = getRandomInteger(minHeight, maxHeight);
         let bar = document.createElement("div");
         bar.className = "bar";
         bar.style.width = barWidth + "%";
@@ -49,12 +51,13 @@ function resize() {
     }
 }
 
-function getRndInteger(min, max) {
+function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 $("#sort-button").click(function() {
     let algorithm = $("#sort-picker").val();
+
 
     switch(algorithm) {
         case "Bubble sort":
@@ -71,6 +74,7 @@ function swap(arr, i, j) {
         arr[i] = arr[j];
         arr[j] = tmp;
 
+        const container = document.getElementById("bars");
         const style1 = window.getComputedStyle(arr[i]);
         const style2 = window.getComputedStyle(arr[j]);
 
@@ -84,21 +88,51 @@ function swap(arr, i, j) {
             setTimeout(() => {
                 container.insertBefore(arr[j], arr[i]);
                 resolve();
-            }, 250);
+            }, 10);
         });
     });
 }
 
 async function bubbleSort(delay = 100) {
     const bars = document.getElementsByClassName("bar");
+    const isSorted = new Array(bars.length);
+    isSorted.fill(false, 0, bars.length);
+
     for(let i = 0; i < bars.length - 1; i++) {
         for(let j = 0; j < bars.length - 1; j++) {
             let barVal = bars[j].clientHeight;
             let nextBarVal = bars[j + 1].clientHeight;
+            
+            if(isSorted[j + 1] === false) {
+                bars[j].style.background = unsortedBarColor;
+            }
+            
+            if(isSorted[j + 1] === false) {
+                bars[j + 1].style.background = unsortedBarColor;
+            }
+
+            await new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, 50);
+            });
 
             if(barVal > nextBarVal) {
                 await swap(bars, j, j + 1);
             }
+
+            if(isSorted[j + 1] === false) {
+                bars[j].style.background = normalBarColor;
+            }
+            
+            if(isSorted[j + 1] === false) {
+                bars[j + 1].style.background = normalBarColor;
+            }
         }
+
+        isSorted[bars.length - i - 1] = true;
+        bars[bars.length - i - 1].style.background = sortedBarColor;
     }
+
+    bars[0].style.background = sortedBarColor;
 };
