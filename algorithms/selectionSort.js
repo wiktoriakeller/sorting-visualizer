b1e7e0e21e33d3fb7e55c2f731dc3ad8b5ce4f11
wiktoriakeller <wiktoriakeller552@gmail.com>
2022-02-23 18:08:45 +0100

    Update algorithm animation

--- algorithms/selectionSort.js
+++ algorithms/selectionSort.js
@@ -1,4 +1,8 @@
-import {swap, wait, wrongOrderColor, correctOrderColor, normalBarColor, sortedBarColor, maxSpeedTime } from "./base.js";
+import {swap, wait, correctOrderColor, normalBarColor, sortedBarColor, maxSpeedTime } from "./base.js";
+
+const minBarColor = "#0066b2";
+const selectedBarColor = "#c98e61";
+const currentBarColor = correctOrderColor;
 
 export function selectionSort({signal}) {
     if(signal?.aborted) {
@@ -15,25 +19,45 @@ export function selectionSort({signal}) {
         let minBarIndex = -1;
         let minVal = -1;
 
-        for(let i = 0; i < (bars.length - 1); i++) {
-            minBarIndex = i;
-            minVal = bars[minBarIndex].clientHeight;
-            try {
+        try {
+            for(let i = 0; i < (bars.length - 1); i++) {
+                minBarIndex = i;
+                minVal = bars[minBarIndex].clientHeight;
+                await wait(bars, i, null, minBarColor, {signal});
+                
                 for(let j = i + 1; j < bars.length; j++) {
+                    await wait(bars, j, null, selectedBarColor, {signal});
+
                     if(bars[j].clientHeight < minVal) {
+                        let color = normalBarColor;
+                        if(minBarIndex === i) {
+                            color = currentBarColor;
+                        }
+
+                        bars[minBarIndex].style.background = color;
+
+                        await wait(bars, j, null, minBarColor, {signal});
                         minBarIndex = j;
                         minVal = bars[minBarIndex].clientHeight;
                     }
+
+                    if(j !== minBarIndex) {
+                        bars[j].style.background = normalBarColor;
+                    }
                 }
-    
+                
                 if(minBarIndex != i) {
                     await swap(bars, i, minBarIndex, {signal});
+                    bars = document.getElementsByClassName("bar");
                 }
+                bars[i].style.background = sortedBarColor;
             }
-            catch(error) {
-                reject(new DOMException("Aborted", "AbortError"));
-                return;
-            }
+
+            bars[bars.length - 1].style.background = sortedBarColor;
+        }
+        catch(error) {
+            reject(new DOMException("Aborted", "AbortError"));
+            return;
         }
         
         resolve();
